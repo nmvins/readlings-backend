9cb24752c69e688ba98c769ad6930cbd50022ec0
Noemi <noemi@example.com>
2022-06-01 12:32:32 +0300

    Mvn changes

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -9,7 +9,7 @@ public class ServerApplication {
     public static void main(String[] args) {
 
         //SpringApplication.run(ServerApplication.class, args);
-        return "Yessss";
+        System.out.println("frs");
     }
 
 }
