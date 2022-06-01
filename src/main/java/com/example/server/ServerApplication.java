a58705c4a4708b7723643077d982bf522a918ebe
Noemi <noemi@example.com>
2022-06-01 12:29:22 +0300

    Mvn changes

--- src/main/java/com/example/server/ServerApplication.java
+++ src/main/java/com/example/server/ServerApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ServerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ServerApplication.class, args);
+
+        //SpringApplication.run(ServerApplication.class, args);
+        return "Yessss";
     }
 
 }
